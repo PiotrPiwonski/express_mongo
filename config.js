fcac565b1f5205650097b7c186fca3fe64956afd
Piotr Piwoński <piwonskipiotrp@gmail.com>
2020-10-08 16:27:32 +0200

    admin login, admin news

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  db:
+    "mongodb+srv://admin:7ZGxqTH2hMJKNzOz@cluster0.h7zq1.mongodb.net/test?retryWrites=true&w=majority",
+  keySession: ["TWOJKLUCZ"],
+  maxAgeSession: 24 * 60 * 60 * 1000,
+};
+
+{
+  /* &w=majority */
+}
